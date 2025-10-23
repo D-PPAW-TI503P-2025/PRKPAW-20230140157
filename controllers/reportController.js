30014c6ad36c55c77043504459313e40b2bc1d48
Yuzhong23 <xixixixifatur@gmail.com>
2025-10-23 14:37:30 +0700

    update tugas

---
+++ controllers/reportController.js
@@ -0,0 +1,8 @@
+    const presensiRecords = require("../Data/presensiData");
+    exports.getDailyReport = (req, res) => {
+    console.log("Controller: Mengambil data laporan harian dari array...");
+    res.json({
+        reportDate: new Date().toLocaleDateString(),
+        data: presensiRecords,
+    });
+    };
